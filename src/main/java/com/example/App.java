898d37d4afef4f6b8fde812890e5539ef0129a19
SHALOM SHARON MOSES <5023152@it.fcrit.ac.in>
2026-02-10 15:05:58 +0530

    ee1

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,28 +1,15 @@
 package com.example;
 
-import java.util.Scanner;
-
 public class App 
 {
     public static void main(String[] args)
     {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        double num1 = sc.nextDouble();
-
-        System.out.print("Enter second number: ");
-        double num2 = sc.nextDouble();
+        double num1 = 10;
+        double num2 = 5;
 
         System.out.println("Addition: " + (num1 + num2));
         System.out.println("Subtraction: " + (num1 - num2));
         System.out.println("Multiplication: " + (num1 * num2));
-
-        if(num2 != 0)
-            System.out.println("Division: " + (num1 / num2));
-        else
-            System.out.println("Division not possible (cannot divide by zero)");
-
-        sc.close();
+        System.out.println("Division: " + (num1 / num2));
     }
 }
